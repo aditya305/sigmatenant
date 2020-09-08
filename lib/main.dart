b318055fdc92048f3cb3918703e25a929e58453e
Aditya Sutar <aditya.sutar305@gmail.com>
2020-09-08 13:05:08 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -30,19 +30,21 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   FetchData fetchData = FetchData();
-
+  Future<void> futureContent;
   List<Content> contentData = <Content>[];
 
-  getContent() async {
+  Future<void> getContent() async {
     var data = await fetchData.getData();
-    for (int i = 0; i < 5; i++) {
-      contentData.add(
-        Content(
-          displayName: data["tags"][i]["displayName"],
-          description: data["tags"][i]["description"],
-          tags: data["tags"][i]["meta"],
-        ),
-      );
+    if (contentData.isEmpty) {
+      for (int i = 0; i < 5; i++) {
+        contentData.add(
+          Content(
+            displayName: data["tags"][i]["displayName"],
+            description: data["tags"][i]["description"],
+            tags: data["tags"][i]["meta"],
+          ),
+        );
+      }
     }
     print(contentData);
   }
@@ -51,7 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    getContent();
+    futureContent = getContent();
   }
 
   @override
@@ -67,79 +69,171 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
-      body: ListView.builder(
-        shrinkWrap: true,
-        itemCount: contentData.length,
-        padding: EdgeInsets.all(17.0),
-        itemBuilder: (context, int index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: AnimatedContainer(
-              color: Colors.white,
-              duration: Duration(
-                milliseconds: 200,
-              ),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey,
-                      offset: Offset(0.0, 1.0), //(x,y)
-                      blurRadius: 6.0,
-                    ),
-                  ],
-                ),
-                padding: EdgeInsets.all(10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: 5.0,
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(7.0),
-                      child: Text(
-                        contentData[index].displayName ?? "",
-                        style: TextStyle(
-                          color: Colors.pink,
-                          fontWeight: FontWeight.bold,
+      body: FutureBuilder(
+        future: futureContent,
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return Text("None");
+            case ConnectionState.waiting:
+              return Center(child: CircularProgressIndicator());
+            case ConnectionState.active:
+              return ListView.builder(
+                shrinkWrap: true,
+                itemCount: contentData.length,
+                padding: EdgeInsets.all(17.0),
+                itemBuilder: (context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: AnimatedContainer(
+                      color: Colors.white,
+                      duration: Duration(
+                        milliseconds: 200,
+                      ),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey,
+                              offset: Offset(0.0, 1.0), //(x,y)
+                              blurRadius: 6.0,
+                            ),
+                          ],
+                        ),
+                        padding: EdgeInsets.all(10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 5.0,
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(7.0),
+                              child: Text(
+                                contentData[index].displayName ?? "",
+                                style: TextStyle(
+                                  color: Colors.pink,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey,
+                                    offset: Offset(0.0, 1.0), //(x,y)
+                                    blurRadius: 6.0,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Container(
+                              child: Text(
+                                contentData[index].description ?? "",
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Container(
+                              child: Text(
+                                contentData[index].tags ?? "",
+                                style: TextStyle(color: Colors.pink),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey,
-                            offset: Offset(0.0, 1.0), //(x,y)
-                            blurRadius: 6.0,
-                          ),
-                        ],
+                    ),
+                  );
+                },
+              );
+            case ConnectionState.done:
+              if (snapshot.hasError) {
+                return Text("Error: ${snapshot.error}");
+              }
+              return ListView.builder(
+                shrinkWrap: true,
+                itemCount: contentData.length,
+                padding: EdgeInsets.all(17.0),
+                itemBuilder: (context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: AnimatedContainer(
+                      color: Colors.white,
+                      duration: Duration(
+                        milliseconds: 200,
+                      ),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey,
+                              offset: Offset(0.0, 1.0), //(x,y)
+                              blurRadius: 6.0,
+                            ),
+                          ],
+                        ),
+                        padding: EdgeInsets.all(10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 5.0,
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(7.0),
+                              child: Text(
+                                contentData[index].displayName ?? "",
+                                style: TextStyle(
+                                  color: Colors.pink,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey,
+                                    offset: Offset(0.0, 1.0), //(x,y)
+                                    blurRadius: 6.0,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Container(
+                              child: Text(
+                                contentData[index].description ?? "",
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Container(
+                              child: Text(
+                                contentData[index].tags ?? "",
+                                style: TextStyle(color: Colors.pink),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Container(
-                      child: Text(
-                        contentData[index].description ?? "",
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Container(
-                      child: Text(
-                        contentData[index].tags ?? "",
-                        style: TextStyle(color: Colors.pink),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          );
+                  );
+                },
+              );
+          }
+          return Container();
         },
       ),
     );
